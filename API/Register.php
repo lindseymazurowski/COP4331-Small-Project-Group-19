3699c89783b5e697d36446d04c85aae5883fa84b
rfarrell961 <54159745+rfarrell961@users.noreply.github.com>
2022-09-07 18:21:34 -0400

    Add files via upload

---
+++ API/Register.php
@@ -0,0 +1,71 @@
+
+<?php
+
+    $inData = getRequestInfo();
+
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $login = $inData["login"];
+    $password = $inData["password"];
+    $regPasswordConf = $inData["regPasswordConf"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+
+        $result = $conn->query("SELECT login FROM users WHERE login='$login'");
+        
+        //checking to see if username already exists
+        if($result->num_rows > 0)
+        {
+            returnWithError("Username Already Exists");
+        }
+        elseif($password != $regPasswordConf)
+        {
+            returnWithError("Passwords Do Not Match");
+        }
+        else
+        {
+            $stmt = $conn->prepare("INSERT into users(firstName, lastName, login, password) VALUES (?,?,?,?)");
+            $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+            $stmt->execute();
+            $last_id = $conn->insert_id;
+            
+            returnWithInfo($firstName, $lastName, $last_id);
+        }
+        
+        $stmt->close();
+        $conn->close();
+
+    }
+   
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
+
+
