cd1f55f195e0a38aaf1fa1af29c634c8cf136cb7
rfarrell961 <54159745+rfarrell961@users.noreply.github.com>
2022-10-01 23:01:44 -0400

    Add files via upload

---
+++ Edit.php
@@ -0,0 +1,44 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+    $ID = $inData["ID"];
+    $newFirst = $inData["firstName"];
+    $newLast = $inData["lastName"];
+    $newEmail = $inData["email"];
+    $newPhone = $inData["phone"];
+
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("UPDATE contacts SET firstName=?,lastName=?,email=?,phone=? WHERE ID=?");
+		$stmt->bind_param("sssss",$newFirst,$newLast,$newEmail,$newPhone,$ID);
+		$stmt->execute();
+        $stmt->close();
+		$conn->close();
+        returnWithError("");
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
